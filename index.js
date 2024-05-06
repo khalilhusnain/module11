e541f3db17e51bd3f2d8dba02b229614c420831f
khalilhusnain <khalilhusnain4@gmail.com>
2024-05-07 01:49:13 +0530

    add

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/notes', (req, res) => {
